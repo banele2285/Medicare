7ddcf12eb96b9361160f4b84e5a87300fe97434c
banele2285 <131881040+banele2285@users.noreply.github.com>
2023-09-08 12:02:02 +0200

    Add files via upload

---
+++ MainController.java
@@ -0,0 +1,15 @@
+package com.phase1.springweb.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class MainController {
+
+	@Controller
+	public	String Index() {
+	return "index.html";
+
+	}
+
+}
